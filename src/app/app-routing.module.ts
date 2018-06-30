97ac6237fab40d2d26943fa7a365e96bf416853b
harshal ganbote <harshal.ganbote@outlook.com>
2018-06-30 18:03:57 +0530

    signin design

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/signin',
+    pathMatch: 'full'
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  }
+];
+
+@NgModule({
+  imports: [
+  RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SigninComponent } from './pages/signin/signin.component';
+import { AppRoutingModule } from './app-routing.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule.forRoot(),
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
